1146ce2683478b57bba2881f07011fef97a82379
Carat89 <carat4work@gmail.com>
2021-03-09 22:46:24 -0500

    bottom_nav

--- app/src/main/java/com/example/navigationcomp/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.navigationcomp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.findNavController
+import androidx.viewbinding.ViewBinding
+import com.example.navigationcomp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        init()
+    }
+
+    private fun init() {
+        mBinding.bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.page_1 -> findNavController(R.id.nav_host_fragment_container).navigate(R.id.to_page_one)
+                R.id.page_2 -> findNavController(R.id.nav_host_fragment_container).navigate(R.id.to_page_two)
+            }
+            true
+        }
     }
 }
